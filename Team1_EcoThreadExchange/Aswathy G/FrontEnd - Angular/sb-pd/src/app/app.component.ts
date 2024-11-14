f9901098a0c64851e83818b46e9a1fd41c193fa9
Your Name <you@example.com>
2024-11-14 18:36:20 +0530

    Initial commit of Angular Project

---
+++ Team1_EcoThreadExchange/Aswathy G/FrontEnd - Angular/sb-pd/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { AuthComponent } from "./auth/auth.component";
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, AuthComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'sb-pd';
+}
