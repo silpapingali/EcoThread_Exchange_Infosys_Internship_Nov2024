786cae6ae2edf0b6941515c9845c841af77216ee
GOPISETTI GOPI <95520388+gopisettigopi123@users.noreply.github.com>
2024-12-05 10:27:48 +0530

    Add files via upload

---
+++ Team5_EcoThreadExchange/Gopisetti Gopi/server/config.js
@@ -0,0 +1,6 @@
+export default {
+    JWT_SECRET : "gopi@123",
+    EMAIL: "steve.franecki@ethereal.email", // testing email & password
+    PASSWORD : "sMf46xCzrvdrxvuagc",
+    ATLAS_URI: "mongodb+srv://gopisettigopi6756:6feC4yJUXscuoq6M@cluster0.kom9w.mongodb.net/gopi?retryWrites=true&w=majority&appName=Cluster0"
+}
